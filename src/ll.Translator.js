49dad2654758c4d2a7da90afe8ccfe138a6ab548
Ed Sanders <ejsanders@gmail.com>
2019-07-01 12:05:57 +0100

    Move (un)bundling & fetchLangPairsPromise up to base class

--- src/ll.Translator.js
+++ src/ll.Translator.js
@@ -12,11 +12,38 @@
  * @constructor
  */
 ll.Translator = function LLTranslator() {
+	this.langPairsPromise = this.fetchLangPairsPromise();
 };
 
 /* Initialization */
 
 OO.initClass( ll.Translator );
+
+/* Static methods */
+
+/**
+ * Bundle groups of strings for translation in one go
+ *
+ * @param {Array[]} groups Array of Arrays of strings
+ * @return {string} The groups, joined with separators that break sentences
+ */
+ll.Translator.static.bundle = function ( groups ) {
+	return groups.map( function ( group ) {
+		return group.join( '\n:!!:\n' );
+	} ).join( '\n:!!!:\n' );
+};
+
+/**
+ * Unbundle groups of strings translated in one go
+ *
+ * @param {string} bundled Bundled groups, as returned by #bundle
+ * @return {Array[]} Array of Arrays of strings
+ */
+ll.Translator.static.unbundle = function ( bundled ) {
+	return bundled.split( /\s*:!!!:\s*/ ).map( function ( groupString ) {
+		return groupString.split( /\s*:!!:\s*/ );
+	} );
+};
 
 /* Instance methods */
 
@@ -34,7 +61,11 @@ ll.Translator.prototype.translate = null;
  * Get available language pairs
  * @return {Promise} Promise resolving with list of [{source:'source',target:'target'}]
  */
-ll.Translator.prototype.getLangPairsPromise = null;
+ll.Translator.prototype.getLangPairsPromise = function () {
+	return this.langPairsPromise;
+};
+
+ll.Translator.prototype.fetchLangPairsPromise = null;
 
 ll.Translator.prototype.getParallelLangPairsPromise = function () {
 	return this.getLangPairsPromise().then( function ( pairs ) {
@@ -51,5 +82,75 @@ ll.Translator.prototype.getParallelLangPairsPromise = function () {
 ll.Translator.prototype.pairSupported = function ( langPairs, source, target ) {
 	return langPairs.some( function ( pair ) {
 		return pair.source === source && pair.target === target;
+	} );
+};
+
+/**
+ * Translate plaintext
+ *
+ * @param {string} sourceLang Source language code
+ * @param {string} targetLang Target language code
+ * @param {string} text The text to translate
+ * @return {Promise} Promise resolving with the translated text
+ */
+ll.Translator.prototype.translatePlaintext = function ( sourceLang, targetLang ) {
+	var translator = this;
+
+	return this.getLangPairsPromise().then( function ( langPairs ) {
+		if ( !translator.pairSupported( langPairs, sourceLang, targetLang ) ) {
+			throw new Error( 'Unsupported language pair "' + sourceLang + '"" > "' + targetLang + '"' );
+		}
+	} );
+};
+
+/**
+ * Translate chunked text
+ *
+ * @param {string} sourceLang Source language code
+ * @param {string} targetLang Target language code
+ * @param {ll.ChunkedText[]} sourceTexts The text to translate
+ * @return {Promise} Promise resolving with the translated text
+ */
+ll.Translator.prototype.translate = function ( sourceLang, targetLang, sourceTexts ) {
+	var plexSourceGroups,
+		translator = this;
+
+	/**
+	 * From chunked text, derive a list of plaintext strings, one with each annotated chunk
+	 * with that chunk upper cased, plus one for the whole string.
+	 *
+	 * For example chunked text corresponding to 'foo <b>bar</b> baz <b>qux</b> quux', return:
+	 * [
+	 *     'foo BAR baz qux quux',
+	 *     'foo bar baz QUX quux',
+	 *     'foo bar baz qux quux'
+	 * ]
+	 * @param {ll.ChunkedText} chunked The chunked text
+	 * @return {string[]} The corresponding plaintext strings
+	 */
+	function getPlexGroup( chunked ) {
+		var outputList = [];
+		chunked.chunks.forEach( function ( chunk ) {
+			outputList.push( chunked.allText.slice( 0, chunk.start ) +
+				chunk.text.toUpperCase() +
+				chunked.allText.slice( chunk.start + chunk.text.length )
+			);
+		} );
+		outputList.push( chunked.allText );
+		return outputList;
+	}
+
+	plexSourceGroups = sourceTexts.map( getPlexGroup );
+
+	return this.translatePlaintext(
+		sourceLang,
+		targetLang,
+		this.constructor.static.bundle( plexSourceGroups )
+	).then( function ( modifiedTargetsBundle ) {
+		var modifiedTargetsList = translator.constructor.static.unbundle( modifiedTargetsBundle );
+		return modifiedTargetsList.map( function ( modifiedTargets, i ) {
+			var targetPlainText = modifiedTargets.pop();
+			return ll.adaptAnnotationsWithModifiedTargets( sourceTexts[ i ], targetPlainText, modifiedTargets );
+		} );
 	} );
 };

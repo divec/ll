985f18e001c16381f87d1a26a3f11f5f8f5d7a06
David Chan <david@troi.org>
2019-07-25 11:38:01 +0100

    On commit, clear ll/update annotation and remove any content with ll/conflict

--- src/ve.dm.Surface.ext.js
+++ src/ve.dm.Surface.ext.js
@@ -4,18 +4,58 @@
  * @copyright 2019 LL team and others; see LICENSE.txt for terms
  */
 
+/* Static properties */
+
+/* Instance methods */
+
 /**
  * Mark a node as approved and store its translation pair as approved
  *
  * @param {ve.dm.ContentBranchNode} node The node
  */
 ve.dm.Surface.prototype.markApproved = function ( node ) {
-	var doc, tx;
-	doc = this.getDocument();
-	tx = ve.dm.TransactionBuilder.static.newFromAttributeChanges(
-		doc, node.getOuterRange().start,
+	var i, start, hasConflict,
+		transactions = [],
+		doc = this.getDocument(),
+		data = doc.data,
+		updateAnnotation = ve.dm.annotationFactory.create( 'll/update' ),
+		conflictAnnotation = ve.dm.annotationFactory.create( 'll/conflict' ),
+		conflictHash = doc.getStore().hash( conflictAnnotation ),
+		conflictSince = null;
+
+	// Unset dirty flag on CBN
+	transactions.push( ve.dm.TransactionBuilder.static.newFromAttributeChanges(
+		doc,
+		node.getOuterRange().start,
 		{ 'll-dirty': 'approved' }
-	);
-	this.change( tx );
+	) );
+	// Remove ll/update annotation
+	transactions.push( ve.dm.TransactionBuilder.static.newFromAnnotation(
+		doc,
+		node.getRange(),
+		'clear',
+		updateAnnotation
+	) );
+	// Remove content with ll/conflict annotation
+	for ( i = node.getRange().end - 1, start = node.getRange().start; i >= start; i-- ) {
+		hasConflict = data.getAnnotationHashesFromOffset( i ).indexOf( conflictHash ) !== -1;
+		if ( conflictSince === null && hasConflict ) {
+			conflictSince = i;
+		} else if ( conflictSince !== null && !hasConflict ) {
+			transactions.push( ve.dm.TransactionBuilder.static.newFromRemoval(
+				doc,
+				new ve.Range( i + 1, conflictSince + 1 )
+			) );
+			conflictSince = null;
+		}
+	}
+	if ( conflictSince !== null ) {
+		transactions.push( ve.dm.TransactionBuilder.newFromRemoval(
+			doc,
+			new ve.Range( start, conflictSince + 1 )
+		) );
+	}
+	// TODO: This should be atomic (or better still, squashed into a single commit)
+	this.change( transactions );
 	doc.storeApprovedPair( node );
 };
